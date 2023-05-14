6f5add8326f0664835310d91c27d1588fa2b1749
mhd7800 <hamoudiouchar@gmail.com>
2023-05-14 19:34:17 +0300

    new changes

--- src/Leetcode/RemoveDuplicates.java
+++ src/Leetcode/RemoveDuplicates.java
@@ -4,17 +4,15 @@ import java.util.Arrays;
 
 public class RemoveDuplicates {
     public static void main(String[] args) {
-        int[] nums = {1,2,2,5,6};
+        int[] nums = {1,2,2,5,8,10};
         removeDuplicates(nums);
     }
 
     public static int removeDuplicates(int[] nums) {
         int counter=1;
         int n = nums.length;
-        int[] newNums = new int[n];
 
-
-            for(int j=0; j<=n-1; j++)
+            for(int j=0; j<n-1; j++)
             {
                 if(nums[j]!=nums[j+1])
                 {
