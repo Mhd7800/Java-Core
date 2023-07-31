689094fd1e7373baa5f97bd6155e74c26ddc0c71
mhd7800 <hamoudiouchar@gmail.com>
2023-07-31 18:27:55 +0300

    new changes

--- src/Leetcode/ValidParantheses.java
+++ src/Leetcode/ValidParantheses.java
@@ -1,52 +1,30 @@
 package Leetcode;
 
-import java.util.HashMap;
 import java.util.Stack;
 
-/*
-Example 1:
 
-Input: s = "()"
-Output: true
-Example 2:
-
-Input: s = "()[]{}"
-Output: true
-Example 3:
-
-Input: s = "(]"
-Output: false
- */
 public class ValidParantheses {
-
     public static void main(String[] args) {
-
         ValidParantheses v = new ValidParantheses();
-        System.out.println(v.isValid("[]{{"));
+        System.out.println(v.isValid("()"));
     }
-    public boolean isValid(String s)
-    {
+
+    public boolean isValid(String s) {
         int n = s.length();
+        Stack<Character> st = new Stack<>();
 
-        Stack<Character> st = new Stack<Character>();
-
-        for (int i=0; i<n; i++)
-        {
-            if(s.charAt(i)=='[' || s.charAt(i)=='(' || s.charAt(i)=='{')
-            {
-                st.push(s.charAt(i));
-            }
-            else
-            {
-                if(st.empty())
-                {
+        for (int i = 0; i < n; i++) {
+            char c = s.charAt(i);
+            if (c == '(' || c == '{' || c == '[') {
+                st.push(c);
+            } else {
+                if (st.empty()) {
                     return false;
                 }
-
-                 if ((s.charAt(i)!=']' && st.peek()!='[') || (s.charAt(i)!='}' && st.peek()!='{') ||  (s.charAt(i)!=')' && st.peek()!='('))
-                 {
-                     return false;
-                 }
+                char top = st.pop();
+                if ((c == ')' && top != '(') || (c == '}' && top != '{') || (c == ']' && top != '[')) {
+                    return false;
+                }
             }
         }
 
